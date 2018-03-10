7c1fb815dba8c40a2ddfdde03c0eec10a251fbf7
Mohamad Ihsan <mohamad_ihsan100@yahoo.co.id>
2018-03-10 23:08:46 +0700

    update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -151,33 +151,6 @@
                     </li>
                 </ul>
                 <ul class="nav-right">
-                    <li class="header-notification lng-dropdown">
-                        <a href="#" id="dropdown-active-item">
-                            <i class="flag-icon flag-icon-gb m-r-5"></i> English
-                        </a>
-                        <ul class="show-notification">
-                            <li>
-                                <a href="#" data-lng="en">
-                                    <i class="flag-icon flag-icon-gb m-r-5"></i> English
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" data-lng="es">
-                                    <i class="flag-icon flag-icon-es m-r-5"></i> Spanish
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" data-lng="pt">
-                                    <i class="flag-icon flag-icon-pt m-r-5"></i> Portuguese
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" data-lng="fr">
-                                    <i class="flag-icon flag-icon-fr m-r-5"></i> French
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
                     <li class="header-notification">
                         <a href="#!">
                             <i class="ti-bell"></i>
